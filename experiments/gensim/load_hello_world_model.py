25684db693ad7612ecd65f82a9c729d2ac50ce8a
aditya-dan <danaditya98@gmail.com>
2025-11-19 10:49:51 -0500

    data, output and experiments are gitignored

--- experiments/gensim/load_hello_world_model.py
+++
@@ -1,4 +0,0 @@
-from gensim.models import Word2Vec
-model = Word2Vec.load("hello_world_word2vec.model")
-# print(model.wv['hello'])
-print(model.wv.most_similar(positive=['girl', 'prince'], negative=['boy']))
