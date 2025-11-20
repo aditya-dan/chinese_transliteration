d544fb64d6dd84dc5537da3bf82b1cca17a5c494
aditya-dan <danaditya98@gmail.com>
2025-11-20 14:59:44 -0500

    RNN encoder and decoder classes taken from PyTorch documentation

---
+++ experiments/rnn/Encoder.py
@@ -0,0 +1,16 @@
+from torch import nn
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, dropout_p=0.1):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, inp):
+        embedded = self.dropout(self.embedding(inp))
+        output, hidden = self.gru(embedded)
+        return output, hidden
