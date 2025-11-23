52a2bfc62fd8a11d2b5118b5b9d39a5e187a783e
aditya-dan <danaditya98@gmail.com>
2025-11-23 16:58:44 -0500

    build_training_data_from_json.sh: Run this script from inside scripts directory to generate training data out of wiki_zh_2019.zip kept inside data/. Later we can change this script to take the source of the corpus as an argument

---
+++ scripts/build_training_data_from_json.sh
@@ -0,0 +1,9 @@
+cd ../data || exit
+unzip wiki_zh_2019.zip
+cd wiki_zh || exit
+
+for dir in ./*; do
+  for file in ${dir}/*; do
+    cat $file | jq ".text" | sed -e 's/^"//' -e 's/"$//' >> ../training_data.txt
+  done
+done
